d2ad427a026776c9c5de509fc838ff186514f39f
Honey Sam <105299685+Honey28Git@users.noreply.github.com>
2024-10-27 21:48:40 +0530

    Delete templates/wsgi.py

--- templates/wsgi.py
+++
@@ -1,5 +0,0 @@
-# wsgi.py
-from app import app  # Replace 'app' with the filename of your Flask app module without the .py extension
-
-if __name__ == "__main__":
-    app.run()  # Only needed if running directly, optional for deployments
